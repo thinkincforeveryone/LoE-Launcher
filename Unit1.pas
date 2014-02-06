7e60af84454e68f7151ad2bd171bb7f5a05e777b
William Rafael <mlpfimbr@gmail.com>
2014-02-06 14:10:50 -0200

    Updates in the interface
    Added some buttons, labels...
    - No functions yet

--- Unit1.pas
+++ Unit1.pas
@@ -4,13 +4,29 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, XPMan, ComCtrls, pngimage, ExtCtrls;
+  Dialogs, XPMan, ComCtrls, pngimage, ExtCtrls, StdCtrls, IdBaseComponent,
+  IdComponent, IdTCPConnection, IdTCPClient, IdHTTP;
 
 type
   TForm1 = class(TForm)
     Image1: TImage;
-    TabControl1: TTabControl;
     XPManifest1: TXPManifest;
+    IdHTTP1: TIdHTTP;
+    Timer1: TTimer;
+    GroupBox1: TGroupBox;
+    Memo1: TMemo;
+    Memo2: TMemo;
+    Memo3: TMemo;
+    GroupBox2: TGroupBox;
+    PageControl1: TPageControl;
+    TabSheet1: TTabSheet;
+    TabSheet2: TTabSheet;
+    TabSheet3: TTabSheet;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    Button1: TButton;
+    ProgressBar1: TProgressBar;
   private
     { Private declarations }
   public
---
+++ Unit1.~pas
@@ -0,0 +1,27 @@
+unit Unit1;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, XPMan, ComCtrls, pngimage, ExtCtrls;
+
+type
+  TForm1 = class(TForm)
+    Image1: TImage;
+    TabControl1: TTabControl;
+    XPManifest1: TXPManifest;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+end.
